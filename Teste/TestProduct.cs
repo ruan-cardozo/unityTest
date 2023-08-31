d972a09d6f74d617525400e63c77385fba3b5472
ruan-cardozo <ruanvcardozo@gmail.com>
2023-08-30 23:07:23 -0300

    update

--- Teste/TestProduct.cs
+++ Teste/TestProduct.cs
@@ -1,43 +1,44 @@
-namespace Teste;
+using Xunit; // Certifique-se de que está usando a biblioteca Xunit para escrever testes
 
-public class TestProduct
+namespace Teste
 {
-    [Fact]
-    public void createObject()
+    public class TestProduct
     {
-        //Arrange - Preparar cenário
-        int code = 1;
-        string name = "Mouse", unity = "PC";
-        float balance = 0, cost = 0;
+        [Fact]
+        public void CreateObject() // Renomeado o método para seguir a convenção de capitalização
+        {
+            // Arrange - Preparar cenário
+            int code = 1;
+            string name = "Mouse", unity = "PC";
+            float balance = 0, cost = 0;
 
-        //Act - Executar
-        Product product = new Product(code, name, unity, balance, cost);
+            // Act - Executar
+            Product product = new Product(code, name, unity, balance, cost);
 
-
-        //Assert - Esperar Resultado
-        Assert.Equal(code, product.Code);
-        Assert.Equal(name, product.Name);
-        Assert.Equal(unity, product.Unity);
-        Assert.Equal(balance, product.Balance);
-        Assert.Equal(cost, product.Cost);
+            // Assert - Esperar Resultado
+            Assert.Equal(code, product.Code);
+            Assert.Equal(name, product.Name);
+            Assert.Equal(unity, product.Unity);
+            Assert.Equal(balance, product.Balance);
+            Assert.Equal(cost, product.Cost);
+        }
     }
-}
 
-public class Product
-{   
+    public class Product
+    {   
         public int Code { get; }
         public string Name { get; }
         public string Unity { get; }
         public float Balance { get; }
         public float Cost { get; }
 
-    public Produto(int code, string name, string unity, float balance, float cost)
-        
-    {
-        Code = code;
-        Name = name;
-        Unity = unity;
-        Balance = balance;
-        Cost = cost;
+        public Product(int code, string name, string unity, float balance, float cost) // Corrigido o nome do construtor
+        {
+            Code = code;
+            Name = name;
+            Unity = unity;
+            Balance = balance;
+            Cost = cost;
+        }
     }
 }
